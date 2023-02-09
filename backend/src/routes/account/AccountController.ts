81696d2e5274ee5f574f2ab3ce54fe5497f2f1c8
Gamblego <garima.10420@gmail.com>
2023-02-09 20:05:38 +0530

    bugfix

--- backend/src/routes/account/AccountController.ts
+++ backend/src/routes/account/AccountController.ts
@@ -5,7 +5,6 @@ import {IReq, IRes} from "@src/constants/AssignmentInterfaces";
 import {IAccount} from "@src/models/Account";
 import {IListResponse} from "@src/schemaobjects/response/IListResponse";
 import HttpStatusCodes from "@src/constants/HttpStatusCodes";
-import {NextFunction} from "express";
 import logger from "jet-logger";
 import AccountService from "@src/services/AccountService";
 import {IAccountSearchRequest} from "@src/schemaobjects/request/IAccountSearchRequest";
@@ -13,7 +12,7 @@ import {TAccountRequest, TAccountResponse} from "@src/schemaobjects/types";
 import {ISaveResponse} from "@src/schemaobjects/response/ISaveResponse";
 
 async function getAll
-(request: IReq<IAccountSearchRequest>, response: IRes, next: NextFunction) : Promise<IRes> {
+(request: IReq<IAccountSearchRequest>, response: IRes) : Promise<IRes> {
     const startTime: number = Date.now();
     const accountSearchRequest: IAccountSearchRequest = request.body;
     const responseBody: IListResponse<IAccount> =
@@ -23,7 +22,7 @@ async function getAll
 }
 
 async function getOne
-(request: IReq, response: IRes, next: NextFunction) : Promise<IRes | void> {
+(request: IReq, response: IRes) : Promise<IRes | void> {
     const startTime: number = Date.now();
     const { accountId } = request.params;
     const account: TAccountResponse = await AccountService.getAccount(accountId);
@@ -31,7 +30,7 @@ async function getOne
     return response.status(HttpStatusCodes.OK).json(account);
 }
 
-async function createOne(request: IReq<TAccountRequest>, response: IRes, next: NextFunction) : Promise<IRes | void> {
+async function createOne(request: IReq<TAccountRequest>, response: IRes) : Promise<IRes | void> {
     const startTime: number = Date.now();
     const accountSaveRequest: TAccountRequest = request.body;
     const responseBody: ISaveResponse = await AccountService.createAccount(accountSaveRequest);
@@ -40,7 +39,7 @@ async function createOne(request: IReq<TAccountRequest>, response: IRes, next: N
 }
 
 async function deleteOne
-(request: IReq, response: IRes, next: NextFunction) : Promise<IRes> {
+(request: IReq, response: IRes) : Promise<IRes> {
     const startTime: number = Date.now();
     const { accountId } = request.params;
     const accountDeleteResponse: ISaveResponse = await AccountService.deleteAccount(accountId);
