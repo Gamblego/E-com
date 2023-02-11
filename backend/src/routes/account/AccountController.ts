389cfca7da4be3e5a949cb4b6336ea2ab8ebf4e1
Gamblego <garima.10420@gmail.com>
2023-02-11 21:17:45 +0530

    account service refactoring

--- backend/src/routes/account/AccountController.ts
+++ backend/src/routes/account/AccountController.ts
@@ -5,37 +5,51 @@ import {IReq, IRes} from "@src/constants/AssignmentInterfaces";
 import {ISessionAccount} from "@src/models/Account";
 import {IListResponse} from "@src/schemaobjects/response/IListResponse";
 import HttpStatusCodes from "@src/constants/HttpStatusCodes";
+import httpStatusCodes from "@src/constants/HttpStatusCodes";
 import logger from "jet-logger";
 import AccountService from "@src/services/AccountService";
 import {TAccountRequest, TAccountResponse, TAccountSearchRequest} from "@src/schemaobjects/types";
 import {ISaveResponse} from "@src/schemaobjects/response/ISaveResponse";
 import {Privilege} from "@src/constants/AssignmentEnums";
 import SessionUtil from "@src/util/SessionUtil";
+import {PromiseWrapper} from "@src/util/AssignmentUtil";
+import {RouteError, USER_UNAUTHORIZED_ERROR} from "@src/helper/Error";
 
 async function getAll
 (request: IReq<TAccountSearchRequest>, response: IRes) : Promise<IRes> {
     const startTime: number = Date.now();
     const accountSearchRequest: TAccountSearchRequest = request.body;
-    const webToken: ISessionAccount | undefined = response.locals.sessionUser;
+    const webToken: ISessionAccount = response.locals.sessionUser!;
+    if(webToken.privilege == Privilege.Client) {
+        throw new RouteError(httpStatusCodes.UNAUTHORIZED, USER_UNAUTHORIZED_ERROR);
+    }
     const responseBody: IListResponse<TAccountResponse> =
-        await AccountService.getAllAccountsMatchingFilter(accountSearchRequest, webToken);
+        await PromiseWrapper(AccountService.getAllAccountsMatchingFilter(accountSearchRequest));
     logger.info(`time taken to complete AccountRouter.GetAll - ${Date.now() - startTime} ms`)
     return response.status(HttpStatusCodes.OK).json(responseBody);
 }
 
 async function getOne
-(request: IReq, response: IRes) : Promise<IRes | void> {
+(request: IReq, response: IRes) : Promise<IRes> {
     const startTime: number = Date.now();
     const { accountId } = request.params;
-    const account: TAccountResponse = await AccountService.getAccount(accountId);
+    const webToken: ISessionAccount = response.locals.sessionUser!;
+    if(webToken.privilege === Privilege.Admin || webToken.accountId === accountId) {
+        throw new RouteError(httpStatusCodes.UNAUTHORIZED, USER_UNAUTHORIZED_ERROR);
+    }
+    const account: TAccountResponse = await PromiseWrapper(AccountService.getAccount(accountId));
     logger.info(`time taken to complete AccountRouter.GetOne - ${Date.now() - startTime} ms`);
     return response.status(HttpStatusCodes.OK).json(account);
 }
 
-async function createOne(request: IReq<TAccountRequest>, response: IRes) : Promise<IRes | void> {
+async function createOne(request: IReq<TAccountRequest>, response: IRes) : Promise<IRes> {
     const startTime: number = Date.now();
     const accountSaveRequest: TAccountRequest = request.body;
-    const responseBody: ISaveResponse = await AccountService.createAccount(accountSaveRequest);
+    const webToken: ISessionAccount = response.locals.sessionUser!;
+    if(webToken.privilege !== Privilege.Admin && webToken.accountId !== '') {
+        throw new RouteError(httpStatusCodes.UNAUTHORIZED, USER_UNAUTHORIZED_ERROR);
+    }
+    const responseBody: ISaveResponse = await PromiseWrapper(AccountService.createAccount(accountSaveRequest));
     const sessionDetails: ISessionAccount = {
         accountId: responseBody.id,
         username: accountSaveRequest.username,
@@ -50,7 +64,11 @@ async function deleteOne
 (request: IReq, response: IRes) : Promise<IRes> {
     const startTime: number = Date.now();
     const { accountId } = request.params;
-    const accountDeleteResponse: ISaveResponse = await AccountService.deleteAccount(accountId);
+    const webToken: ISessionAccount = response.locals.sessionUser!;
+    if(webToken.privilege !== Privilege.Admin && webToken.accountId !== '') {
+        throw new RouteError(httpStatusCodes.UNAUTHORIZED, USER_UNAUTHORIZED_ERROR);
+    }
+    const accountDeleteResponse: ISaveResponse = await PromiseWrapper(AccountService.deleteAccount(accountId));
     SessionUtil.clearCookie(response);
     logger.info(`time taken to complete AccountRouter.CreateOne - ${Date.now() - startTime} ms`);
     return response.status(HttpStatusCodes.OK).json(accountDeleteResponse);
