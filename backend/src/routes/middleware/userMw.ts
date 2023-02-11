f30711aeec834b3248c6eafd2aa0c33d598d7c6f
Gamblego <garima.10420@gmail.com>
2023-02-11 18:48:52 +0530

    bugfix related to jwt and error handling

--- backend/src/routes/middleware/userMw.ts
+++ backend/src/routes/middleware/userMw.ts
@@ -2,14 +2,16 @@
  * Middleware to verify user logged in
  */
 
-import { Request, Response, NextFunction } from 'express';
-import { JwtPayload } from 'jsonwebtoken';
+import {NextFunction, Request, Response} from 'express';
+import {JwtPayload} from 'jsonwebtoken';
 
 import HttpStatusCodes from '@src/constants/HttpStatusCodes';
 
 import SessionUtil from '@src/util/SessionUtil';
-import { ISessionAccount } from '@src/models/Account';
+import {ISessionAccount} from '@src/models/Account';
 import {RouteError, USER_UNAUTHORIZED_ERROR} from "@src/helper/Error";
+import {Privilege} from "@src/constants/AssignmentEnums";
+import logger from "jet-logger";
 
 // **** Types **** //
 
@@ -30,16 +32,19 @@ async function userMw(
     // Get session data
     const sessionData = await SessionUtil.getSessionData<TSessionData>(req);
     // Set session data to locals
-    if (
-        typeof sessionData === 'object' &&
-        'privilege' in sessionData
+    if(typeof sessionData === undefined) {
+      res.locals.sessionUser = {
+        username: '',
+        privilege: Privilege.Client,
+        accountId: ''
+      };
+    } else if (
+        typeof sessionData === 'object'
     ) {
       res.locals.sessionUser = sessionData;
-      return next();
-      // Return an unAuth error if user is not an admin
-    } else {
-      throw new RouteError(HttpStatusCodes.UNAUTHORIZED, USER_UNAUTHORIZED_ERROR);
+      logger.info(`cookie for incoming request : ${JSON.stringify(sessionData)}`);
     }
+    return next();
   } catch (err: any) {
     next(err);
   }
